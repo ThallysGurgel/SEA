df29e48eea561266cc5ef2c7b4ad1f2ef9a4e5d4
Thallys Gurgel <thallysgurgel@hotmail.com>
2024-03-18 09:42:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+class MobileNavBar {
+    constructor(inteface) {
+        this.menu-desktop = document.querySelector(menu-desktop);
+        this.mobile-button = ddocument.querySelector(mobile-button);
+    }
+    addClickEvent() {
+        this.mobile-button.addEventListener("Click")
+    };
+}
